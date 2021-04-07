3a1e258359242b45c5caed140b106cc8c87b2b56
Roni Serra <roniblizz87@gmail.com>
2021-04-07 20:31:05 -0300

    :tada: desafio ok

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,20 +16,43 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
   }
+  setTasks( oldState => [...oldState, newTask]);
+  setNewTaskTitle('')
+
+  } 
+ 
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTask = tasks.map(task => task.id === id ?{
+      ...task,
+      isComplete: !task.isComplete
+
+    } : task);
+
+    setTasks(newTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(tasks => tasks.id !== id);
+
+    setTasks(filteredTasks)
+
+
   }
 
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minhas Tarefas:</h2>
 
         <div className="input-group">
           <input 
